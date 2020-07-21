8bbc5098007147d0dd6abfb0a15b4499a7075c3c
pr1266 <pourya_1999@yahoo.com>
2020-07-21 13:05:21 -0700

    'gradiant'

--- main.m
+++ main.m
@@ -9,7 +9,25 @@ h = 1200;
 w = 1920;
 patch_size = sqrt((h * w) / 40);
 
-first_corner = output_image(1:patch_size,1:patch_size,:);
+%first_corner = output_image(1:patch_size,1:patch_size,:);
+
+
+my_array = cell(numel(data_dir), 2); 
+
+
+for image = 9 : numel(data_dir) - 1
+    
+    picture = rgb2gray(imread([data_path data_dir(image).name]));
+    [Gmag, Gdir] = imgradient(picture);
+    my_array(image - 8, 1) = mat2cell(Gmag, patch_size, patch_size);
+    my_array(image - 8, 2) = mat2cell(Gdir, patch_size, patch_size);
+    
+end
+    
+
+
+
+
 
 imshow(first_corner, []);
 
