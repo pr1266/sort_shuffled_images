796472a3fe0df69c93d39fcfc344c1eb9b49a538
pr1266 <pourya_1999@yahoo.com>
2020-07-21 13:48:49 -0700

    'first_patch'

--- main.m
+++ main.m
@@ -12,8 +12,7 @@ patch_size = sqrt((h * w) / 40);
 %first_corner = output_image(1:patch_size,1:patch_size,:);
 
 
-my_array = cell(numel(data_dir), 2); 
-
+my_array = cell(numel(data_dir), 3); 
 
 for image = 9 : numel(data_dir) - 1
     
@@ -21,16 +20,39 @@ for image = 9 : numel(data_dir) - 1
     [Gmag, Gdir] = imgradient(picture);
     my_array(image - 8, 1) = mat2cell(Gmag, patch_size, patch_size);
     my_array(image - 8, 2) = mat2cell(Gdir, patch_size, patch_size);
+    my_array(image - 8, 3) = cellstr(data_dir(image).name);
     
 end
-    
+
+x_temp_1 = cell2mat(my_array(1, 1));
+x_temp_2 = cell2mat(my_array(1, 2));
+top_corner_1 = x_temp_1(1,:);
+left_corner_1 = x_temp_1(:,1);
+right_corner_1 = x_temp_1(:,patch_size);
+bottom_corner_1 = x_temp_1(patch_size,:);
 
 
 
+top_corner_2 = x_temp_2(1,:);
+left_corner_2 = x_temp_2(:,1);
+right_corner_2 = x_temp_2(:,patch_size);
+bottom_corner_2 = x_temp_2(patch_size,:);
 
+corner = rgb2gray(imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Corner_1_1.tif'));
 
-imshow(first_corner, []);
+[GV, GD] = imgradient(corner);
 
-corner = imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Corner_1_1.tif');
+right_corner_corner = GV(:,patch_size);
 
-x = mse(corner, first_corner);
+for i = 1:36
+    %disp(i);
+    x_temp = cell2mat(my_array(i,1));
+    left_corner = x_temp(:,1);
+    %if mse(left_corner, right_corner_corner) < 1
+        disp(mse(left_corner, right_corner_corner));
+    %end
+end
+
+patch_10 = imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Patch_10.tif');
+output_image(1: patch_size, patch_size + 1: 2 * patch_size, :) = patch_10;
+imshow(output_image, []);
