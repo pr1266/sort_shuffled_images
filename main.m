565b6dfbeb195dc51d5426faeee46920dbeb0afa
pr1266 <pourya_1999@yahoo.com>
2020-07-21 15:12:25 -0700

    'for-loop-1'

--- main.m
+++ main.m
@@ -62,7 +62,33 @@ top_corner_5_8 = GV(1,:);
 for i = 1 : patch_size : w
     for j = 1 : patch_size : h
         
+        %% corner e aval :
         if i == 1 && j == 1
+            error = [];
+            %% inja right corner barash darmiarim :
+            %% hame error haro darmiarim va min e error haro barash dar nazar migirim :
+            for k = 1 : 36
+                
+                element = cell2mat(my_array(k, 1));
+                disp(k);
+                left_gradient = element(:, 1);
+                MSE = mse(left_gradient, right_corner_1_1);
+                error = [error MSE];
+            end
+            
+            minimum = min(error);
+            min_error = find(error == minimum);
+            disp(min_error);
+            
+            %% hala index ro darim miaim tasvir ro ba indexesh az my_array mikhonim va gharar midim :
+            new_picture = cell2mat(my_array(min_error, 3));
+            
+            disp(size(new_picture));
+            
+            
+            output_image(1:patch_size, patch_size + 1: 2 * patch_size, :) = new_picture;
+            figure(1);
+            imshow(output_image, []);
             
         end
         
