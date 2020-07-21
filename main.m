fb7e7d091171b198e73f054698234b8a148c1039
pr1266 <pourya_1999@yahoo.com>
2020-07-21 14:41:27 -0700

    'corners'

--- main.m
+++ main.m
@@ -2,57 +2,71 @@
 data_path = 'C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\';
 data_dir = dir(data_path);
 
-%array = cell(numel(data_dir), 16)
-
+%% output image ro mikhoonim ke badan patch haro toosh sort konim :
 output_image = imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Output.tif');
+%% size tasvir :
 h = 1200;
 w = 1920;
+
+%% bar asas e size e tasvir size har patch ro bedast miarim :
 patch_size = sqrt((h * w) / 40);
 
-%first_corner = output_image(1:patch_size,1:patch_size,:);
+%% ye array dorost mikonim ta etelaat e patch haro zakhire konim :
+my_array = cell(numel(data_dir), 4);
 
-
-my_array = cell(numel(data_dir), 3); 
+%% etelaat ro be array montaghel mikonim :
 
 for image = 9 : numel(data_dir) - 1
     
-    picture = rgb2gray(imread([data_path data_dir(image).name]));
-    [Gmag, Gdir] = imgradient(picture);
+    picture = imread([data_path data_dir(image).name]);
+    temp_picture = rgb2gray(picture);
+    [Gmag, Gdir] = imgradient(temp_picture);
+    
+    %% khod e tasvir, esmesh, meghdar gradientesh va direction e gradientesh :
     my_array(image - 8, 1) = mat2cell(Gmag, patch_size, patch_size);
     my_array(image - 8, 2) = mat2cell(Gdir, patch_size, patch_size);
-    my_array(image - 8, 3) = cellstr(data_dir(image).name);
+    my_array(image - 8, 3) = mat2cell(picture, patch_size, patch_size, 3);
+    my_array(image - 8, 4) = cellstr(data_dir(image).name);
     
 end
 
-x_temp_1 = cell2mat(my_array(1, 1));
-x_temp_2 = cell2mat(my_array(1, 2));
-top_corner_1 = x_temp_1(1,:);
-left_corner_1 = x_temp_1(:,1);
-right_corner_1 = x_temp_1(:,patch_size);
-bottom_corner_1 = x_temp_1(patch_size,:);
+%% inja etelaat e corner haro darmiarim :
+
+%% corner e 1 :
+corner_1_1 = rgb2gray(imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Corner_1_1.tif'));
+[GV, GD] = imgradient(corner_1_1);
+right_corner_1_1 = GV(:,patch_size);
+bottom_corner_1_1 = GV(patch_size, :);
+
+%% corner e 2 :
+corner_1_8 = rgb2gray(imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Corner_1_8.tif'));
+[GV, GD] = imgradient(corner_1_8);
+left_corner_1_8 = GV(:,1);
+bottom_corner_1_8 = GV(patch_size,:);
+
+%% corner e 3 :
+corner_5_1 = rgb2gray(imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Corner_5_1.tif'));
+[GV, GD] = imgradient(corner_5_1);
+right_corner_5_1 = GV(:,patch_size);
+top_corner_5_1 = GV(1,:);
+
+%% corner e 4 :
+corner_5_8 = rgb2gray(imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Corner_5_8.tif'));
+[GV, GD] = imgradient(corner_5_8);
+left_corner_5_8 = GV(:,1);
+top_corner_5_8 = GV(1,:);
 
 
+%% inja mikham tasvir ro por konam :
 
-top_corner_2 = x_temp_2(1,:);
-left_corner_2 = x_temp_2(:,1);
-right_corner_2 = x_temp_2(:,patch_size);
-bottom_corner_2 = x_temp_2(patch_size,:);
-
-corner = rgb2gray(imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Corner_1_1.tif'));
-
-[GV, GD] = imgradient(corner);
-
-right_corner_corner = GV(:,patch_size);
-
-for i = 1:36
-    %disp(i);
-    x_temp = cell2mat(my_array(i,1));
-    left_corner = x_temp(:,1);
-    %if mse(left_corner, right_corner_corner) < 1
-        disp(mse(left_corner, right_corner_corner));
-    %end
+for i = 1 : patch_size : w
+    for j = 1 : patch_size : h
+        
+        if i == 1 && j == 1
+            
+        end
+        
+    end
 end
 
-patch_10 = imread('C:\Users\pr1266\Desktop\cv_project\Puzzle_1_40\Patch_10.tif');
-output_image(1: patch_size, patch_size + 1: 2 * patch_size, :) = patch_10;
-imshow(output_image, []);
+
